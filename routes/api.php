ff29b1692cd78a4ddda9465e2f9a6a9aabac6bb4
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-11-23 14:04:02 +0200

    removed unused apis

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\NewsController;
 
-Route::prefix("/courses")->group(function() {
+Route::prefix("/courses")->group(function () {
 	Route::post("/", [NewsController::class, "post_news"]);
 	Route::put("/{id}", [NewsController::class, "edit_news"]);
 	Route::delete("/{id}", [NewsController::class, "delete_news"]);
 	Route::get("/", [NewsController::class, "get_news"]);
-	Route::post("/{id}", [NewsController::class,"add_article"]);
-	Route::post("/{id}/request", [NewsController::class, "request_news"]);
-	Route::get("/{id}/articles", [NewsController::class, "get_articles"]);
-	Route::post("/{id}/attachment", [NewsController::class, "upload_news_attachment"]);
-	Route::post("/{id}/articles/attachment", [NewsController::class, "upload_article_attachment"]);
+	Route::post("/{id}", [NewsController::class, "add_article"]);
 });
